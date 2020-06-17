30214c369fb0318a20b57eb240d03a5ed19bea09
lexjames06 <ajs.stewart@btinternet.com>
2020-06-17 14:29:35 +0100

    Add cart icon and cartDropdown components to application. Also add cart reducer

---
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { toggleCartVisibility } from '../../redux/cart/cart.actions';
+
+import { ReactComponent as ShoppingIcon } from '../../assets/cart.svg';
+
+import './cart-icon.styles.scss';
+
+const CartIcon = ({ toggleCartVisibility }) => (
+    <div className='cart-icon' onClick={toggleCartVisibility}>
+        <ShoppingIcon className='shopping-icon' />
+        <span className='item-count'>0</span>
+    </div>
+);
+
+const mapDispatchToProps = dispatch => ({
+    toggleCartVisibility: () => dispatch(toggleCartVisibility()),
+});
+
+export default connect(null, mapDispatchToProps)(CartIcon);
---
+++ src/redux/cart/cart.types.js
@@ -0,0 +1,3 @@
+export const CartActionTypes = {
+    TOGGLE_CART_DROPDOWN_VISIBILITY: 'TOGGLE_CART_DROPDOWN_VISIBILITY'
+};
