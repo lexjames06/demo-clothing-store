34cd50a61ba2ecc3a6e6690d78f36bc630d28c32
lexjames06 <ajs.stewart@btinternet.com>
2020-06-26 16:46:30 +0100

    Update cart utils to update cart item reducer and create cart item componenet and connected cart dropdown with cart item reducer

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -2,4 +2,9 @@ import { CartActionTypes } from './cart.types';
 
 export const toggleCartVisibility = () => ({
     type: CartActionTypes.TOGGLE_CART_DROPDOWN_VISIBILITY,
+});
+
+export const addItem = item => ({
+    type: CartActionTypes.ADD_ITEM,
+    payload: item,
 })
